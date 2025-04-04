e8a78b0010963089ee8d1f3dc2a9602ceb5e66e6
Silviu Ojog <sil.ojog@gmail.com>
2025-04-04 19:30:08 +0300

    Validare CNP

--- Curs5/cititor/club/urls.py
+++ Curs5/cititor/club/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 
-from .views import validare_cnp_view
+from .views import validare_cnp_view,rezultat_cnp_view
 
 ## URL MIC
 urlpatterns = [
-  path('cnp', validare_cnp_view)
+  path('cnp', validare_cnp_view),
+  path('rezultat', rezultat_cnp_view)
 ]
--- Curs5/cititor/club/views.py
+++ Curs5/cititor/club/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 
 def validare_cnp_view(request):
@@ -8,3 +8,39 @@ def validare_cnp_view(request):
     }
     return render(request, "formular_cnp.html", context)
 
+def valideaza_cnp(cnp:str):
+    if not cnp.isnumeric():
+        return False
+    if len(cnp) != 13:
+        return False
+    
+    gen = int(cnp[0])
+    if gen not in range(1, 9):
+        return False
+    
+    an = int(cnp[1:3])
+    today = datetime.now()
+
+    if gen in (5, 6) and (2000 + an) > today.year:
+        return False
+    
+    luna = int(cnp[3:5])
+    if luna not in range(1, 13):
+        return False
+    
+    zi = int(cnp[5:7])
+    if zi not in range(1, 32):
+        return False
+
+    return True
+
+
+def rezultat_cnp_view(request):
+    print(request.POST)
+    cnp = request.POST.get('cnp')
+    print("CNP-ul primit este:", cnp)
+
+    context = {
+        'valid': valideaza_cnp(cnp)
+    }
+    return render(request, "rezultat_cnp.html", context)
