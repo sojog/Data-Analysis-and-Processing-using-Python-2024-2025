966b9de3a4546962b7fc47fcc608671245063590
Silviu Ojog <sil.ojog@gmail.com>
2025-04-25 20:46:00 +0300

    Todo: django_helper with decorator

--- Curs7/django_helper.py
+++ Curs7/django_helper.py
@@ -19,38 +19,63 @@ else:
     python_command = "python3"
 
 
-def create_project(project_name="test_project"):
+def create_project(project_name="test_project", *applications):
     CREATE_PROJECT_CMD = f"{python_command} -m django startproject {project_name}"
     subprocess.call(CREATE_PROJECT_CMD, shell=True)
+    time.sleep(1)
+
+    for app_name in applications:
+        create_application(app_name=app_name, project_name=project_name)
 
 def delete_project(project_name="test_project"):
     shutil.rmtree(project_name)
 
 
-def create_application(app_name="test_app", project_name="test_project"):
-    # Current working directory
-    cwd = os.getcwd()
-    print("Inainte de a schimba path-ul", cwd)
-    ## imi schimba path-ul
-    os.chdir( os.path.join(cwd, project_name))
-    print("Dupa schimbarea path-ului", os.getcwd())
+def project_path(project_name="test_project"):
+    return os.path.join(os.getcwd(), project_name)
 
-    CREATE_APP_CMD  = f"{python_command} manage.py startapp {app_name}"
+def inner_project_path(project_name="test_project"):
+    return os.path.join(os.getcwd(), project_name, project_name)
+
+def app_path(app_name="test_app", project_name="test_project"):
+    return os.path.join(os.getcwd(), project_name, app_name)
+
+
+def change_cwd(path):
+    def change_back_cwd(function):
+        def inner_function(*args, **kwargs):
+            cwd = os.getcwd()
+            print(kwargs)
+            if kwargs:
+                result = function(*args, **kwargs)
+            elif args:
+                result = function(app_name=args[0],project_name= args[1])
+            os.chdir(cwd)
+            return result
+        return inner_function
+    return change_back_cwd
+
+
+@change_cwd(project_path(project_name="test_project"))
+def create_application(app_name="test_app", project_name="test_project"):
+
+    os.chdir(project_path(project_name))
+    
+    CREATE_APP_CMD  = f"{python_command} {project_name}/manage.py startapp {app_name}"
     subprocess.call(CREATE_APP_CMD, shell=True)
+    time.sleep(0.5)
+    setup_application(app_name, project_name)
+
+
+def setup_application(app_name="test_app", project_name="test_project"):
+    _add_app_to_installed_apps(app_name, project_name)
+    _create_templates_folder(app_name, project_name)
+    _create_app_url_file(app_name, project_name)
+    _link_app_in_project_url_file(app_name, project_name)
 
 def _add_app_to_installed_apps(app_name="test_app", project_name="test_project"):
-    # Current working directory
-    cwd = os.getcwd()
-    print("Inainte de a schimba path-ul", cwd)
-    ## imi schimba path-ul
-    os.chdir( os.path.join(cwd, project_name))
-    print("Dupa schimbarea path-ului", os.getcwd())
 
-    cwd = os.getcwd()
-    os.chdir( os.path.join(cwd, project_name))
-    print("Dupa schimbarea path-ului", os.getcwd())
-
-    with open("settings.py", "r") as freader:
+    with open(f"{inner_project_path(project_name)}/settings.py", "r") as freader:
         settings_content = freader.readlines()
         # print(settings_content)
 
@@ -66,27 +91,21 @@ def _add_app_to_installed_apps(app_name="test_app", project_name="test_project")
 
     print("\n".join(settings_content))
     
-    with open("settings.py", "w") as fwriter:
+    with open(f"{inner_project_path(project_name)}/settings.py", "w") as fwriter:
         fwriter.writelines(settings_content)
 
 def _create_templates_folder(app_name="test_app", project_name="test_project"):
-    cwd = os.getcwd()
-    os.chdir(os.path.join(cwd, project_name, app_name))
-    TEMPLATES = "templates"
+    TEMPLATES = f"{app_path(app_name, project_name)}/templates"
     os.makedirs(TEMPLATES, exist_ok=True)
 
 def _create_app_url_file(app_name="test_app", project_name="test_project"):
-    cwd = os.getcwd()
-    os.chdir(os.path.join(cwd, project_name, app_name))
-    URLS_FILE_NAME = "urls.py"
+    URLS_FILE_NAME = f"{app_path(app_name, project_name)}/urls.py"
     URLS_CONTENT = """from django.urls import path\n\nurlpatterns = [\n\n]\n"""
     with open(URLS_FILE_NAME, "w") as fwriter:
         fwriter.write(URLS_CONTENT)
 
 def _link_app_in_project_url_file(app_name="test_app", project_name="test_project"):
-    cwd = os.getcwd()
-    os.chdir(os.path.join(cwd, project_name, project_name))
-    with open("urls.py", "r") as freader:
+    with open(f"{inner_project_path(project_name)}/urls.py", "r") as freader:
         urls_lines = freader.readlines()
 
     print(urls_lines)
@@ -104,7 +123,7 @@ def _link_app_in_project_url_file(app_name="test_app", project_name="test_projec
             urls_lines.insert(index, new_line)
             break
 
-    with open("urls.py", "w") as fwriter:
+    with open(f"{inner_project_path(project_name)}/urls.py", "w") as fwriter:
         fwriter.writelines(urls_lines)
 
 
@@ -118,10 +137,9 @@ def demo_enumerate():
     print(lista_mea)
 
 if __name__ == "__main__":
-    # print(os.getcwd())
-    _link_app_in_project_url_file()
 
-    # create_project()
+    create_project("proiectulmeu", "app1", "app2")
+    # create_application("app3", "proiectulmeu")
     # time.sleep(3)
     # create_application()
     # _add_app_to_installed_apps()
